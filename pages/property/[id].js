3d4e0ab46d15eebaf95bcbf6dc31b3ba4302850c
tisho <72041800+happpystory@users.noreply.github.com>
2022-02-12 20:25:39 +0200

    update

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -3,7 +3,7 @@ import {FaBed, FaBath} from 'react-icons/fa';
 import {BsGridFill} from 'react-icons/bs';
 import {GoVerified} from 'react-icons/go';
 import millify from "millify";
-import { baseUrl, fetchApi } from '../../utils/fetchAPI';
+import { baseUrl, fetchApi } from '../../utils/fetchApi';
 import ImageScrollbar from '../../components/ImageScrollbar';
 
 const PropertyDetails = ({propertyDetails: {price, rentFrequency, rooms, title, baths, area, agency, isVerified, description, type, purpose, furnishingStatus, amenities, photos}}) => (
